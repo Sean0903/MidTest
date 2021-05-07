ed116c361bc180ff39541fbec0287713ec7c45fe
Sean0903 <ms0421204@gmail.com>
2021-05-07 11:48:59 +0800

    finish layout

---
+++ app/src/main/java/com/sean/publisher/data/Article.kt
@@ -0,0 +1,9 @@
+package com.sean.publisher.data
+
+class Article (
+    val title: String?,
+    val author: String?,
+    val time: String?,
+    val content: String?,
+    val category: String?
+)
